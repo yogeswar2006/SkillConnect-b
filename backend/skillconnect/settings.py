39743ff93021e60655694487c3d776d7bad584ad
yogeswar2006 <yogi8247322760@gmail.com>
2026-02-13 23:38:12 +0530

    Ready to build

--- backend/skillconnect/settings.py
+++ backend/skillconnect/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(os.path.join(BASE_DIR,'.env'))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY =os.getenv('SECRECT_KEY') 
+SECRET_KEY =os.getenv('SECRECT_KEY',"unsafe-build-secret-key") 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
